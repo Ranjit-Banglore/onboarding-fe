9154154375916b6c776c68199820adb88ac42451
Ranjit <ranjitkumarkiit@gmail.com>
2021-06-12 17:11:24 +0200

    changed post call to axios

--- src/client/OnboardingClient.js
+++ src/client/OnboardingClient.js
@@ -1,25 +1,14 @@
+import axios from "axios";
+
 const baseUrl = "https://onboarding-be.herokuapp.com/onboarding/v1/signup";
 
-export default function signupPost(inputData, path) {
+export default function signupPost(inputData) {
   let postData = {};
   postData.email = inputData.email;
   postData.password = inputData.password;
-  fetch(baseUrl, {
-    method: "POST", // or 'PUT'
-    mode: "cors",
-    supportHeaderParams: true,
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: JSON.stringify(postData),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("data:", data);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+  axios({
+    method: "post",
+    url: baseUrl,
+    data: postData,
+  });
 }
